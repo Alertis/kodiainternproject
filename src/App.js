b53bdeee40ed513948687105efaa5354a7985f5e
Alertis <osmanyavuzdemir96@gmail.com>
2019-05-24 00:53:46 +0300

    redux, semantic-ui and react-router-dom libraries were imported

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Main from './pages/Main';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Route exact path="/App" component={Main}></Route>
+
+      </div>
+    );
+  }
 }
 
 export default App;
